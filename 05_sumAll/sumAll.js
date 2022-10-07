f90c1e52be9aafcb3f855b441c6c878920a238f0
dawkk <gventuri14@gmail.com>
2022-10-07 12:45:40 -0300

    Another one bites the dust, finished sumAll tests!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+  let sum = 0
+  if(arg1 < 0 || arg2 < 0){ 
+    return 'ERROR'
+  } else if (typeof arg1 != "number" || typeof arg2 != "number"){
+    return 'ERROR'
+    } else {
+      if(arg1 < arg2) {
+        for(let i = arg1; i <= arg2; i++) {
+          // console.log("valor atual de sum é:", sum, "valor atual de i é:", i)
+          sum = sum + i;
+        } 
+        console.log("entered first loop", sum);
+        return sum;
+      } else if (arg1 > arg2) {
+        for(let j = arg2; j <= arg1; j++) {
+          // console.log("valor atual de sum é:", sum, "valor atual de j é:", j)
+          sum = sum + j;
+        }
+        console.log("entered second loop", sum);
+        return sum;
+      }
+    }
 };
 
 // Do not edit below this line
