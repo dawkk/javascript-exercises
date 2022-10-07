3e030101bcfe247a9b05e338940dbce9bedf7b07
dawkk <gventuri14@gmail.com>
2022-10-07 11:42:38 -0300

    Quite hard to solve, will revisit this exercise in a few days

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const array = args[0];
+  return array.filter(val => !args.includes(val))
 
+  
 };
+
+// this passed first test
+//return arr.filter(f => f !== item)
+
+//an attempt of logic to go through all the array results and match it with the removable results and splice whichever matches - it failed :(
+  // let fullArray =  Array.from(arguments);
+  // let removableItems = fullArray.filter(f => f !== arr);
+  // console.log("These are removable", removableItems);
+  // console.log("This is full array", fullArray);
+
+  // for(let i = arr.length -1; i > 0; i--) {
+  //   for(let j = removableItems.length -1; i > 0; i--) {
+  //     if(arr[i] === removableItems[j]){
+  //       arr.splice(i, 1);
+  //     }
+  //   }
+  //   console.log(arr)
+  // }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
