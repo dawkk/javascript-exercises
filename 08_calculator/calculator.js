3a1a7fdfb879789dca593ffc96cb1c4fc898ef83
dawkk <gventuri14@gmail.com>
2022-10-08 11:22:14 -0300

    Finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(arg1, arg2) {
+	let result = arg1 + arg2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1, arg2) {
+	let result = arg1 - arg2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(...theArgs) {
+	let total = 0;
+  const array = theArgs;
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array[i].length; j++) {
+      total += array[i][j];
+    }
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(...theArgs) {
+  let total = 1;
+  const array = theArgs;
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array[i].length; j++) {
+      total *= array[i][j];
+    }
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let total = 1;
+  for(let i = 0; i < power; i++) {
+   total *= number;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0) {
+    return 1;
+  }else{
+      return (number * factorial(number -1));
+  }
 };
 
 // Do not edit below this line
