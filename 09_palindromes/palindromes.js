3c79ef6f1e6a3e3a46f2ca68f30be4140f1706b1
dawkk <gventuri14@gmail.com>
2022-10-24 01:56:32 -0300

    Finally with help of FreeCodeCamp managed to complete this one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+  // Regex to remove all other characters unwanted let regex = /[\W_]/g
+  let removesSpecialCharRegex = /[^A-Za-z0-9]/g;
+  let formatWord = word.toLowerCase().replace(removesSpecialCharRegex, '');
+  let wordBackwards = formatWord.split('').reverse().join('');
+
+  return wordBackwards === formatWord;
 };
 
 // Do not edit below this line
