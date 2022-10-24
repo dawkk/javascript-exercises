011996e3e39494d3f2e8b15b0f2bb1a549a68977
dawkk <gventuri14@gmail.com>
2022-10-24 02:31:57 -0300

    Adds fibonacci resolution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+  let lastNumber = 0;
+  let currentNumber = 1;
+  let sumNumber = +position;
+  let positionToNumber = +position;
+
+  console.log(position)
+
+if(position < 0) {
+  return "OOPS";
+  
+} else {
+  for(let i = 2; i <= positionToNumber; i++) {
+    sumNumber = lastNumber + currentNumber;
+    lastNumber = currentNumber;
+    currentNumber = sumNumber;
+  }
+  return sumNumber;
+};
+  
 };
 
 // Do not edit below this line
